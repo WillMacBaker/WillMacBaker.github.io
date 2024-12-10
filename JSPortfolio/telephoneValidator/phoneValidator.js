ba99714bba8c495a169b3fe3f56ff0e94c07791f
WillMacBaker <williammacluskie@yahoo.co.uk>
2024-12-10 15:10:34 +0000

    adding more initial page functionality and test js script

---
+++ JSPortfolio/telephoneValidator/phoneValidator.js
@@ -0,0 +1,7 @@
+
+
+document.getElementById(".check-btn").addEventListener("click", function() {
+    alert("Hooray!");
+  });
+
+ // document.querySelector('#button').addEventListener('onclick', exit_alert);
