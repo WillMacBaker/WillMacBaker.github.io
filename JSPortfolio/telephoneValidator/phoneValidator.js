f10d27e8234625d133946d3d2f12a41c51d61837
WillMacBaker <williammacluskie@yahoo.co.uk>
2024-12-10 16:35:09 +0000

    planning and making start on restructuring javascript for functionality

--- JSPortfolio/telephoneValidator/phoneValidator.js
+++ JSPortfolio/telephoneValidator/phoneValidator.js
@@ -1,7 +1,15 @@
+const inputField = document.getElementById('user-input').value;
 
 
-document.getElementById(".check-btn").addEventListener("click", function() {
-    alert("Hooray!");
-  });
+
+const checkNameInput = () => {
+    if (!inputField){
+        alert("Please provide a phone number")
+    }
+}
+
 
  // document.querySelector('#button').addEventListener('onclick', exit_alert);
+
+ // Refer to https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures-v8/learn-localstorage-by-building-a-todo-app/step-68
+ // for guidance and support
