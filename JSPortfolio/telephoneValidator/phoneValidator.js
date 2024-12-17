2c760c83b38e3e6d8827e65ac72c18f4621d11f2
WillMacBaker <williammacluskie@yahoo.co.uk>
2024-12-17 17:01:24 +0000

    Updating page functionality to append child textNodes to div element, and handle basic validation of valid/invalid values

--- JSPortfolio/telephoneValidator/phoneValidator.js
+++ JSPortfolio/telephoneValidator/phoneValidator.js
@@ -3,7 +3,12 @@ const checkPhoneNumberButton = document.getElementById('check-btn')
 const clearButton = document.getElementById('clear-btn')
 const resultsDividerArea = document.getElementById('results-div')
 
-const phoneNumberRegex = /^(?:1\s*\(?(\d{3})\)?|)(\d{3})(\-|\s+)(\d{4})(\-|\s+)(\d{4})$/;
+// This creates a blank p tag entry, that will be used to populate the
+// results-div
+const pNode = document.createElement("p")
+//^(?:1\s*\(?(\d{3})\)?|)(\d{3})(\-|\s+)(\d{4})(\-|\s+)(\d{4})$/;
+
+const phoneNumberRegex = /(\d)/
 
 checkPhoneNumberButton.addEventListener("click", ()=>{
     console.log(inputField.value)
@@ -11,15 +16,44 @@ checkPhoneNumberButton.addEventListener("click", ()=>{
         alert("Please provide a phone number");
         return;
     }
-    else {
-        // RUN MAIN CHECKING CODE HERE, THIS IS WHERE MEAT OF FUNCTION OCCURS.
+    else if(inputField.value) {
+        console.log(`Adding ${inputField.value} to the regexChecker function`)
+        regexChecker(inputField.value)
     }
 }, false)
 
+
+// This function checks if the number value matches the regex rule.
+const regexChecker = (valueToCheck) => {
+    console.log("RegexChecker function activated!")
+    if(phoneNumberRegex.test(valueToCheck)){
+        console.log(phoneNumberRegex.test(valueToCheck))
+        console.log('Text entered matches regex pattern')
+        addElement(true, valueToCheck)
+    }
+    else if (!phoneNumberRegex.test(valueToCheck)){
+        console.log(phoneNumberRegex.test(valueToCheck))
+        console.log('Text entered does NOT match regex pattern')
+        addElement(false, valueToCheck)
+    }
+}
+
+
+const addElement = (flag, inputValue) => {
+    const placeholderTextInvalid = `Invalid US number: ${inputValue}`
+    const placeholderTextValid = `Valid US number: ${inputValue}`
+
+    if (flag){
+        pNode.textContent = placeholderTextValid
+        
+        resultsDividerArea.appendChild(document.createTextNode(pNode.textContent))//document.createTextNode(placeholderTextValid))
+    }
+    else if (!flag){
+        pNode.textContent = placeholderTextInvalid
+        resultsDividerArea.appendChild(document.createTextNode(pNode.textContent))//document.createTextNode(placeholderTextInvalid))
+    }
+}
+
 clearButton.addEventListener("click", ()=> {
-    console.log("Clear button hit")
-    resultsDividerArea.value == ""
+    resultsDividerArea.textContent = '';
 }, false)
-
-
-// https://codepen.io/misou/pen/VwKKwjL
