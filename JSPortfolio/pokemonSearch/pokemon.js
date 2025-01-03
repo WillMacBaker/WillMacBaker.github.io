42fbf7ec3945bf73d581ceda7d212ff56776523c
WillMacBaker <williammacluskie@yahoo.co.uk>
2025-01-03 13:37:13 +0000

    further additions to data handling + half-baked implementation of updatePage function

--- JSPortfolio/pokemonSearch/pokemon.js
+++ JSPortfolio/pokemonSearch/pokemon.js
@@ -16,50 +16,91 @@ const pokemonSpeed = document.getElementById('speed')
 const inputField = document.getElementById("search-input")
 const searchButton = document.getElementById("search-button")
 
+// Array that will store received pokemon data
+let pokemonDataArray = [];
 
+// Parses input entered into pokemon search box, and returns a url with the 
+// pokemon string parsed and appended, or the pokemon number id appended to end of url
+// query to freecodecamp pokemon api endpoint
+const parseInput = () => {
+    const stringAndIntregex = /^(?=.*[0-9])(?=.*[A-Za-z])[A-Za-z0-9]+$/;
+    
+    // Test functions to check type of entered value.
+    const stringAndIntTest = (string) => {
+        return stringAndIntregex.test(string)
+    }
+    
+    const hasNumber = (string) => {
+        return /\d/.test(string)
+    }
+
+    // If entry value contains both string and int, and is likely 
+    // to break request, don't process further, and throw out alert
+    if (stringAndIntTest(inputField.value) === true){
+        console.log(inputField.value + "contains string and int values!")
+        alert("Please enter either the pokemon id OR the name, not both")
+        return false
+    }
+
+    // If entered value is just a string, begin parsing out values, dash-separate, parse
+    // gender symbols
+    else if (hasNumber(inputField.value) === false ){
+        // treat entry as string and parse accordingly
+        console.log("input is of type integer?: " + hasNumber(inputField.value))
+        // parse and clean string, respectively
+
+
+        return `https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/${inputField.value}`
+    }
+    // If entered value is just an integer, simply append to end of url, return to be parsed
+    else if (hasNumber(inputField.value) === true){
+        console.log("input is of type integer?: " + hasNumber(inputField.value))
+        return `https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/${inputField.value}`
+        
+    }
+    
+}
 
 // Storage variables
 
 searchButton.addEventListener("click", () => {
     // Request variables
-    const fetchRequestUrl = `https://pokeapi-proxy.freecodecamp.rocks/api/pokemon/${inputField.value}`
-    const testRequest = `https://pokeapi-proxy.freecodecamp.rocks/api/pokemon`
-    const rawData = fetch(fetchRequestUrl)
-    .then()
-    const pokemonData = rawData.json()
-    console.log(inputField.value)
-    console.log(fetchRequestUrl)
-    console.log(rawData)
-    console.log(pokemonData)
+
+    if (inputField.value === ""){
+        console.log("Blank entry")
+    }
+    else {
+        const fetchRequestUrl = parseInput()
+        if (fetchRequestUrl === false){
+            console.log("Multi-type entry detected, aborting url request...")
+        }
+        else {
+            // Make request to endpoint, await reponse, return data in json format...
+            function getpokemondata(url){
+                fetch(url)
+                .then((response) => response.json())
+                .then((data) => {
+                    //Updating page and doing things needs to happen here.
+                    pokemonDataArray = data;
+                    console.log("pokemonData array is: ", pokemonDataArray)
+                    updatePage(pokemonDataArray)
+            })
+                .catch((err) => {
+                    console.error(`There was an error: ${err}`)
+                });
+            }
+            // Function that updates page elements based upon received json data back.
+            const updatePage = (pokemon) => {
+                pokemon.forEach(({name, id, weight, height, types, stats, }) => {
+                    console.log("name:", name)
+                    pokemonName.innerText += name
+                    pokemonId.innerText += id
+                    pokemonWeight.innerText += weight
+                    pokemonHeight.innerText += height
+                    // UPDATE PAGE HERE.
+                });
+            }
+            getpokemondata(fetchRequestUrl)
+        }
+    }
 })
-
-
-
-/*
-checkPhoneNumberButton.addEventListener("click", ()=>{
-    console.log(inputField.value)
-    if(inputField.value === ""){
-        alert("Please provide a phone number");
-        return;
-    }
-    else if(inputField.value) {
-        console.log(`Adding ${inputField.value} to the regexChecker function`)
-        regexChecker(inputField.value)
-    }
-}, false)
-
-// This function checks if the number value matches the regex rule.
-const regexChecker = (valueToCheck) => {
-    console.log("RegexChecker function activated!")
-    if(phoneNumberRegex.test(valueToCheck)){
-        console.log(phoneNumberRegex.test(valueToCheck))
-        console.log('Text entered matches regex pattern')
-        addElement(true, valueToCheck)
-    }
-    else if (!phoneNumberRegex.test(valueToCheck)){
-        console.log(phoneNumberRegex.test(valueToCheck))
-        console.log('Text entered does NOT match regex pattern')
-        addElement(false, valueToCheck)
-    }
-}
-*/
